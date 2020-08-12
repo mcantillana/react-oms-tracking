eab7aa938898039c0a23b51d2e07df75f73520b3
Miguel Cantillana <mcantillana@portal53.cl>
2020-08-11 21:08:56 -0400

    se agrego ENV URL

--- src/services/tracking.api.js
+++ src/services/tracking.api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-// export const config = {
-//     apiUrl: process.env.REACT_APP_BASE_API_URL
-// }
+export const config = {
+    apiUrl: process.env.REACT_APP_BASE_API_URL
+}
 // eyJvcmRlcl9pZCI6IDEyMzQsICJlbWFpbCI6ICJtY2FudGlsbGFuYUBwb3J0YWw1My5jbCIgfQ
 
 export const getTracking = async (data) => {
     
     const url = `https://oms-qa.d2.e2l.dev/sales/order/track`;
     
-    return await axios.post(url, {
+    return await axios.post(config.apiUrl, {
         headers: {
             'Content-Type': 'application/json',
         },
